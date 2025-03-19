5b92081a295ef7544afdc1bee26be718f235db93
alex-forza <alexforza62@gmail.com>
2025-03-19 23:09:05 +0300

    Delete locators directory

--- locators/history_page_locators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class HistoryPageLocators:
-    logout_button = [By.XPATH, ".//button[text()='Выход']"]
-    history_orders = [By.XPATH, ".//a[@class='Account_link__2ETsJ text text_type_main-medium text_color_inactive']"]
-    feed_orders = [By.XPATH, ".//p[text()='Лента Заказов']"]
-    last_order = [By.XPATH, ".//div/div/ul/li[last()]"]
-    last_order_number = [By.XPATH, ".//div/div/ul/li[last()]//p"]
-    close_modal_window = [By.XPATH, ".//section[@class='Modal_modal_opened__3ISw4 Modal_modal__P3_V5']//button[@class='Modal_modal__close_modified__3V5XS Modal_modal__close__TnseK']"]
--- locators/registartion_page_locators.py
+++
@@ -1,9 +0,0 @@
-from selenium.webdriver.common.by import By
-
-class RegistrationPageLocators:
-    email_input = [By.XPATH, ".//label[text()='Email']"]
-    name_input = [By.XPATH, ".//label[text()='Имя']"]
-    password_input = [By.XPATH, ".//label[text()='Пароль']"]
-    registration_form = [By.XPATH, ".//div/main/div/form"]
-    already_registration = [By.XPATH, ".//p[text()='Уже зарегистрированы?']"]
-    link_already_registration = [By.XPATH, ".//a[text()='Войти']"]
